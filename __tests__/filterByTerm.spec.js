c9c76ff69a23f6e203b6d7f120ce1984b6d7810e
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2023-08-01 21:13:48 -0600

    Define the expected output, add comments

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,3 +1,5 @@
+// Per the specification, the function under the test should leave out the objects whose url property does not match the given search term
+
 describe("Filter function", () => {
   test("it should filter by a search term (link)", () => {
     const input = [
@@ -5,5 +7,8 @@ describe("Filter function", () => {
       { id: 2, url: "https://www.url2.dev" },
       { id: 3, url: "http://www.link3.dev" },
     ];
+
+    // expected output: an array with a single object, given 'link' as the search term
+    const output = [{ id: 3, url: "https://www.link3.dev" }];
   });
 });
