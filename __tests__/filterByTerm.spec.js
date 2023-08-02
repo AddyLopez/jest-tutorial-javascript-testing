aefee4b325d64b6781e2b060eefcf6e653b7de2d
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2023-08-01 21:27:40 -0600

    Fix typo and Reference Error from 1st failed test

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,11 +1,14 @@
-// Per the specification, the function under the test should leave out the objects whose url property does not match the given search term
+const filterByTerm = (inputArr, searchTerm) => {
+  return inputArr.filter((arrayElement) => arrayElement.url.match(searchTerm));
+};
 
+// Per the specification, the function under the test should leave out the objects whose url property does not match the given search term
 describe("Filter function", () => {
   test("it should filter by a search term (link)", () => {
     const input = [
       { id: 1, url: "https://www.url1.dev" },
       { id: 2, url: "https://www.url2.dev" },
-      { id: 3, url: "http://www.link3.dev" },
+      { id: 3, url: "https://www.link3.dev" },
     ];
 
     // expected output: an array with a single object, given 'link' as the search term
